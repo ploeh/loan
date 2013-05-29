ce8cbacc014f0945a8f3da0b95336b1cbee72872
Mark Seemann <mark@ploeh.dk>
2013-05-29 07:05:01 +0200

    Added Contact and Address data classes.
    While I do realize that the structure of these classes is very
    constraining, and doesn't properly model how addresses and contacts work
    in the real world, I hope they'll be sufficient for demonstration
    purposes.

---
+++ Loan/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.Loan
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ Loan/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.Loan
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
