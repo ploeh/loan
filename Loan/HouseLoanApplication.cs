4aa99ae551180cbe782e926c4a8777ed175b65cc
Mark Seemann <mark@ploeh.dk>
2013-05-29 07:10:04 +0200

    Added HouseLoanApplication,
    which will be used as the main data collection class for collecting data
    about an application for a house loan. This is most likely not the correct
    terminology in the eyes of a Domain Expert, but I hope it's
    understandable.

---
+++ Loan/HouseLoanApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.Loan
+{
+    public class HouseLoanApplication
+    {
+        public HouseLoanApplication()
+        {
+            this.AdditionalApplicants = new List<Contact>();
+        }
+
+        public Contact PrimaryApplicant { get; set; }
+        public ICollection<Contact> AdditionalApplicants { get; private set; }
+    }
+}
