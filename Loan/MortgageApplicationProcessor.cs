2861247f720ad654c5d4d0c8c37cb9715efb9e10
Mark Seemann <mark@ploeh.dk>
2013-06-03 14:51:42 +0200

    Added MortgageApplicationProcessor,
    although it doesn't yet do anything. This is intended to exemplify the
    main business logic of software.

---
+++ Loan/MortgageApplicationProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.Samples.Loan.DataCollection;
+using Ploeh.Samples.Loan.Render;
+
+namespace Ploeh.Samples.Loan
+{
+    public class MortgageApplicationProcessor
+    {
+        IEnumerable<IRendering> ProduceOffer(MortgageApplication application)
+        {
+            var renderings = new List<IRendering>();
+
+            return renderings;
+        }
+    }
+}
