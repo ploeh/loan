533409f9e20e59ba254bb2b1dddadfb7854c5245
Mark Seemann <mark@ploeh.dk>
2013-06-03 16:20:27 +0200

    Added introductury (lorem ipsum) paragraph
    to offer letter.

--- Loan/MortgageApplicationProcessor.cs
+++ Loan/MortgageApplicationProcessor.cs
@@ -41,6 +41,11 @@ namespace Ploeh.Samples.Loan
                     application.PrimaryApplicant.Contact.Address.Country));
             renderings.Add(new LineBreakRendering());
 
+            renderings.Add(
+                new TextRendering(
+                    "It gives us great pleasure to lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
+            renderings.Add(new LineBreakRendering());
+
             return renderings;
         }
     }
