5c9473a43a009300cb6ed8ee6cc7bf6d28d7d888
Mark Seemann <mark@ploeh.dk>
2013-06-04 14:56:03 +0200

    Introduced CompositeMortgageApplicationProcessor.

---
+++ Loan.UnitTest/CompositeMortgageApplicationProcessorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Ploeh.Samples.Loan;
+
+namespace Ploeh.Samples.Loan.UnitTest
+{
+    public class CompositeMortgageApplicationProcessorTests
+    {
+        [Fact]
+        public void SutIsMortgageApplicationProcessor()
+        {
+            var sut = new CompositeMortgageApplicationProcessor();
+            Assert.IsAssignableFrom<IMortgageApplicationProcessor>(sut);
+        }
+    }
+}
