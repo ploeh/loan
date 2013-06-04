9438c64cb12c780d627c64fde8aa33e961a712f1
Mark Seemann <mark@ploeh.dk>
2013-06-04 16:11:44 +0200

    Introduced DateAndLocationMortgageApplicationPrcr,
    which will produce appropriate renderings for a date and location letter
    head.

---
+++ Loan.UnitTest/DateAndLocationMortgageApplicationProcessorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Ploeh.Samples.Loan;
+
+namespace Ploeh.Samples.Loan.UnitTest
+{
+    public class DateAndLocationMortgageApplicationProcessorTests
+    {
+        [Fact]
+        public void SutIsMortgageApplicationProcessor()
+        {
+            var sut = new DateAndLocationMortgageApplicationProcessor();
+            Assert.IsAssignableFrom<IMortgageApplicationProcessor>(sut);
+        }
+    }
+}
---
+++ Loan/DateAndLocationMortgageApplicationProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.Samples.Loan
+{
+    public class DateAndLocationMortgageApplicationProcessor : IMortgageApplicationProcessor
+    {
+        public IEnumerable<Render.IRendering> ProduceOffer(DataCollection.MortgageApplication application)
+        {
+            yield break;
+        }
+    }
+}
