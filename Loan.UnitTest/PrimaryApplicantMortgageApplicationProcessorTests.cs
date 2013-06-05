81f9a83cb98727e3e1b2a74a87128491089d67aa
Mark Seemann <mark@ploeh.dk>
2013-06-05 14:38:26 +0200

    Introduced PrimaryApplicantMortgageApplication[etc
    to render the Primary Applicant.

---
+++ Loan.UnitTest/PrimaryApplicantMortgageApplicationProcessorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Ploeh.Samples.Loan;
+
+namespace Ploeh.Samples.Loan.UnitTest
+{
+    public class PrimaryApplicantMortgageApplicationProcessorTests
+    {
+        [Fact]
+        public void SutIsMortgageApplicationProcessor()
+        {
+            var sut = new PrimaryApplicantMortgageProcessor();
+            Assert.IsAssignableFrom<IMortgageApplicationProcessor>(sut);
+        }
+    }
+}
