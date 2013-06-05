9e81854847b33d77a519c8fdaab8a9c0d17298f7
Mark Seemann <mark@ploeh.dk>
2013-06-05 15:18:22 +0200

    Introduced AnyAdditionalApplicantsSpecification,
    which will encapsulate a check of the existence of any additional
    applicants.

---
+++ Loan/AnyAdditionalApplicantsSpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.Samples.Loan.DataCollection;
+
+namespace Ploeh.Samples.Loan
+{
+    public class AnyAdditionalApplicantsSpecification : IMortgageApplicationSpecification
+    {
+        public bool IsSatisfiedBy(MortgageApplication application)
+        {
+            return false;
+        }
+    }
+}
