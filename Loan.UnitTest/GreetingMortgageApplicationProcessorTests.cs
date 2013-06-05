2cf12bb4e54c70ab801544965c9da9abf13de676
Mark Seemann <mark@ploeh.dk>
2013-06-05 10:28:14 +0200

    Overrode Equals on Greeting.
    Since the class has no state, two instances of the same type are
    considered equal. The hashcode is just a pseudo-random number I banged in
    on the keyboard without thinking.

--- Loan.UnitTest/GreetingMortgageApplicationProcessorTests.cs
+++ Loan.UnitTest/GreetingMortgageApplicationProcessorTests.cs
@@ -62,5 +62,27 @@ namespace Ploeh.Samples.Loan.UnitTest
             };
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void SutEqualsOther()
+        {
+            var sut = new GreetingMortgageApplicationProcessor();
+            var other = new GreetingMortgageApplicationProcessor();
+
+            var actual = sut.Equals(other);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void SutDoesNotEqualAnonymousObject()
+        {
+            var sut = new GreetingMortgageApplicationProcessor();
+            var anonymous = new object();
+
+            var actual = sut.Equals(anonymous);
+
+            Assert.False(actual);
+        }
     }
 }
