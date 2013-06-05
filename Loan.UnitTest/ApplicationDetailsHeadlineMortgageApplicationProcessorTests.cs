d724f2a2b59fdac31ba4c795b536ba6a81159484
Mark Seemann <mark@ploeh.dk>
2013-06-05 10:31:50 +0200

    Introduced ApplicationDetailsHeadline[etc.],
    which will simply render a headline.

---
+++ Loan.UnitTest/ApplicationDetailsHeadlineMortgageApplicationProcessorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Ploeh.Samples.Loan;
+
+namespace Ploeh.Samples.Loan.UnitTest
+{
+    public class ApplicationDetailsHeadlineMortgageApplicationProcessorTests
+    {
+        [Fact]
+        public void SutIsMortgageApplicationProcessor()
+        {
+            var sut = 
+                new ApplicationDetailsHeadlineMortgageApplicationProcessor();
+            Assert.IsAssignableFrom<IMortgageApplicationProcessor>(sut);
+        }
+    }
+}
---
+++ Loan/ApplicationDetailsHeadlineMortgageApplicationProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.Samples.Loan.DataCollection;
+using Ploeh.Samples.Loan.Render;
+
+namespace Ploeh.Samples.Loan
+{
+    public class ApplicationDetailsHeadlineMortgageApplicationProcessor :
+        IMortgageApplicationProcessor
+    {
+        public IEnumerable<IRendering> ProduceOffer(MortgageApplication application)
+        {
+            yield break;
+        }
+    }
+}
