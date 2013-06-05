c0700d4d56e9d923f24199b6bf5fdae22958ba55
Mark Seemann <mark@ploeh.dk>
2013-06-05 22:18:00 +0200

    Introduced CurrentPropertyMortgageApplication[etc]
    to render the current property.

---
+++ Loan.UnitTest/CurrentPropertyMortgageApplicationProcessorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Ploeh.Samples.Loan;
+
+namespace Ploeh.Samples.Loan.UnitTest
+{
+    public class CurrentPropertyMortgageApplicationProcessorTests
+    {
+        [Fact]
+        public void SutIsMortgageApplicationProcessor()
+        {
+            var sut = new CurrentPropertyMortgageApplicationProcessor();
+            Assert.IsAssignableFrom<IMortgageApplicationProcessor>(sut);
+        }
+    }
+}
