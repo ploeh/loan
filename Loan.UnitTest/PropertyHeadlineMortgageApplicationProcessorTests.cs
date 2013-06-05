3eaef1b0339655f45692a162f0c177914983951e
Mark Seemann <mark@ploeh.dk>
2013-06-05 22:34:16 +0200

    Added PropertyHeadlineMortgageApplication[etc].

---
+++ Loan.UnitTest/PropertyHeadlineMortgageApplicationProcessorTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.Samples.Loan;
+using Ploeh.Samples.Loan.DataCollection;
+using Ploeh.Samples.Loan.Render;
+using Xunit;
+
+namespace Ploeh.Samples.Loan.UnitTest
+{
+    public class PropertyHeadlineMortgageApplicationProcessorTests
+    {
+        [Fact]
+        public void SutIsMortgageApplicationProcessor()
+        {
+            var sut = new PropertyHeadlineMortgageApplicationProcessor();
+            Assert.IsAssignableFrom<IMortgageApplicationProcessor>(sut);
+        }
+
+        [Fact]
+        public void ProduceOfferReturnsCorrectResult()
+        {
+            var sut = new PropertyHeadlineMortgageApplicationProcessor();
+
+            var dummyApplication = new MortgageApplication();
+            var actual = sut.ProduceOffer(dummyApplication);
+
+            var expected = new[] 
+            {
+                new Heading2Rendering("Property")
+            };
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SutEqualsOther()
+        {
+            var sut = new PropertyHeadlineMortgageApplicationProcessor();
+            var other = new PropertyHeadlineMortgageApplicationProcessor();
+
+            var actual = sut.Equals(other);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void SutDoesNotEqualAnonymousObject()
+        {
+            var sut = new PropertyHeadlineMortgageApplicationProcessor();
+            var anonymous = new object();
+
+            var actual = sut.Equals(anonymous);
+
+            Assert.False(actual);
+        }
+    }
+}
---
+++ Loan/PropertyHeadlineMortgageApplicationProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.Samples.Loan.DataCollection;
+using Ploeh.Samples.Loan.Render;
+
+namespace Ploeh.Samples.Loan
+{
+    public class PropertyHeadlineMortgageApplicationProcessor : IMortgageApplicationProcessor
+    {
+        public IEnumerable<IRendering> ProduceOffer(MortgageApplication dummyApplication)
+        {
+            yield return new Heading2Rendering("Property");
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is PropertyHeadlineMortgageApplicationProcessor;
+        }
+
+        public override int GetHashCode()
+        {
+            return 7768;
+        }
+    }
+}
