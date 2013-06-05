4933ea129faf51eb2737e26be1b49d8a737eee04
Mark Seemann <mark@ploeh.dk>
2013-06-05 22:44:51 +0200

    Introduced DesiredLoanMortgageApplicationProcessor,
    which will render desired loan characteristics.

---
+++ Loan/DesiredLoanMortgageApplicationProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.Samples.Loan.DataCollection;
+using Ploeh.Samples.Loan.Render;
+
+namespace Ploeh.Samples.Loan
+{
+    public class DesiredLoanMortgageApplicationProcessor : IMortgageApplicationProcessor
+    {
+        public IEnumerable<IRendering> ProduceOffer(MortgageApplication application)
+        {
+            yield break;
+        }
+    }
+}
