38fd34cf57e9f45df38638a341cd1e085353f55b
Mark Seemann <mark@ploeh.dk>
2013-06-05 22:14:35 +0200

    Added PropertyProcessor,
    which is a concrete class that can produce renderings for a Property
    instance (equivalent to ApplicantProcessor).

---
+++ Loan.UnitTest/PropertyProcessorTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Ploeh.Samples.Loan;
+using Ploeh.Samples.Loan.DataCollection;
+using Ploeh.Samples.Loan.Render;
+
+namespace Ploeh.Samples.Loan.UnitTest
+{
+    public class PropertyProcessorTests
+    {
+        [Theory]
+        [InlineData("Main Street 7", "14873 Anywhere", "Norway", 184820, 285)]
+        [InlineData("Side Street 5", "8888 Somewhere", "Norway", 992829, 567)]
+        public void ProduceRenderingsReturnsCorrectResult(
+            string street,
+            string postalCode,
+            string country,
+            int price,
+            int size)
+        {
+            var sut = new PropertyProcessor();
+            var property = new Property
+            {
+                Address = new Address
+                {
+                    Street = street,
+                    PostalCode = postalCode,
+                    Country = country
+                },
+                Price = price,
+                Size = size
+            };
+
+            IEnumerable<IRendering> actual = sut.ProduceRenderings(property);
+
+            var expected = new IRendering[]
+            {
+                new BoldRendering("Address:"),
+                new TextRendering(
+                    " " +
+                    street + ", " +
+                    postalCode + ", " +
+                    country + ". "),
+                new LineBreakRendering(),
+                new BoldRendering("Estimated sales price:"),
+                new TextRendering(" " + price),
+                new LineBreakRendering(),
+                new BoldRendering("Size:"),
+                new TextRendering(" " + size + " square meters"),
+                new LineBreakRendering()
+            };
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SutEqualsOther()
+        {
+            var sut = new PropertyProcessor();
+            var other = new PropertyProcessor();
+
+            var actual = sut.Equals(other);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void SutDoesNotEqualAnonymousObject()
+        {
+            var sut = new PropertyProcessor();
+            var anonymous = new object();
+
+            var actual = sut.Equals(anonymous);
+
+            Assert.False(actual);
+        }
+    }
+}
---
+++ Loan/PropertyProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.Samples.Loan.DataCollection;
+using Ploeh.Samples.Loan.Render;
+
+namespace Ploeh.Samples.Loan
+{
+    public class PropertyProcessor
+    {
+        public IEnumerable<IRendering> ProduceRenderings(Property property)
+        {
+            yield return new BoldRendering("Address:");
+            yield return new TextRendering(
+                " " +
+                property.Address.Street + ", " +
+                property.Address.PostalCode + ", " +
+                property.Address.Country + ". ");
+            yield return new LineBreakRendering();
+
+            yield return new BoldRendering("Estimated sales price:");
+            yield return new TextRendering(" " + property.Price);
+            yield return new LineBreakRendering();
+
+            yield return new BoldRendering("Size:");
+            yield return new TextRendering(" " + property.Size + " square meters");
+            yield return new LineBreakRendering();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is PropertyProcessor;
+        }
+
+        public override int GetHashCode()
+        {
+            return 999;
+        }
+    }
+}
