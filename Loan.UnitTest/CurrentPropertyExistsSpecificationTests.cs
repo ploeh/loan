fcef009785865481c88418439edcf571ed1472ce
Mark Seemann <mark@ploeh.dk>
2013-06-05 21:46:20 +0200

    Introduced CurrentPropertyExistsSpecification,
    which will be used to check for the existence of the CurrentProperty
    property.

---
+++ Loan.UnitTest/CurrentPropertyExistsSpecificationTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Ploeh.Samples.Loan;
+
+namespace Ploeh.Samples.Loan.UnitTest
+{
+    public class CurrentPropertyExistsSpecificationTests
+    {
+        [Fact]
+        public void SutIsMortgageApplicationSpecification()
+        {
+            var sut = new CurrentPropertyExistsSpecification();
+            Assert.IsAssignableFrom<IMortgageApplicationSpecification>(sut);
+        }
+    }
+}
