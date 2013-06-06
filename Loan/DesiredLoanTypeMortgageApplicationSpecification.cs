ea17371b8b6569ce991e62d5518eed422c91665e
Mark Seemann <mark@ploeh.dk>
2013-06-06 11:12:21 +0200

    Introduced DesiredLoanTypeMortgageApplication[etc]

---
+++ Loan/DesiredLoanTypeMortgageApplicationSpecification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.Samples.Loan.DataCollection;
+
+namespace Ploeh.Samples.Loan
+{
+    public class DesiredLoanTypeMortgageApplicationSpecification : 
+        IMortgageApplicationSpecification
+    {
+        public bool IsSatisfiedBy(MortgageApplication application)
+        {
+            return false;
+        }
+    }
+}
