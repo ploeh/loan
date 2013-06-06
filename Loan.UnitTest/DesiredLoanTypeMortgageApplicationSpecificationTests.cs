71575548632e368e4aec524c7c3e6c3fb15b9cd5
Mark Seemann <mark@ploeh.dk>
2013-06-06 11:19:13 +0200

    Implemented DesiredLoanTypeMortgageApplication[etc

--- Loan.UnitTest/DesiredLoanTypeMortgageApplicationSpecificationTests.cs
+++ Loan.UnitTest/DesiredLoanTypeMortgageApplicationSpecificationTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Ploeh.Samples.Loan;
+using Ploeh.Samples.Loan.DataCollection;
 
 namespace Ploeh.Samples.Loan.UnitTest
 {
@@ -15,6 +16,29 @@ namespace Ploeh.Samples.Loan.UnitTest
         {
             var sut = new DesiredLoanTypeMortgageApplicationSpecification();
             Assert.IsAssignableFrom<IMortgageApplicationSpecification>(sut);
+        }
+
+        [Theory]
+        [InlineData(LoanType.InterestOnly, LoanType.InterestOnly, true)]
+        [InlineData(LoanType.FixedRateAnnuity, LoanType.InterestOnly, false)]
+        [InlineData(LoanType.FixedRateAnnuity, LoanType.FixedRateAnnuity, true)]
+        public void IsSatisfiedByReturnsCorrectResult(
+            LoanType matchingLoanType,
+            LoanType desiredLoanType,
+            bool expected)
+        {
+            var sut = new DesiredLoanTypeMortgageApplicationSpecification
+            {
+                MatchingLoanType = matchingLoanType
+            };
+            var application = new MortgageApplication
+            {
+                DesiredLoanType = desiredLoanType
+            };
+
+            var actual = sut.IsSatisfiedBy(application);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Loan/DesiredLoanTypeMortgageApplicationSpecification.cs
+++ Loan/DesiredLoanTypeMortgageApplicationSpecification.cs
@@ -9,9 +9,13 @@ namespace Ploeh.Samples.Loan
     public class DesiredLoanTypeMortgageApplicationSpecification : 
         IMortgageApplicationSpecification
     {
+        public LoanType MatchingLoanType;
+
         public bool IsSatisfiedBy(MortgageApplication application)
         {
-            return false;
+            return object.Equals(
+                this.MatchingLoanType,
+                application.DesiredLoanType);
         }
     }
 }
