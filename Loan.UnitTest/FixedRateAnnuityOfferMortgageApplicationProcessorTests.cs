d828cc45fa75f542004c6ecf67d7fce319b8b857
Mark Seemann <mark@ploeh.dk>
2013-06-06 11:46:25 +0200

    Implemented FixedRateAnnuityOfferMortgage[etc]

--- Loan.UnitTest/FixedRateAnnuityOfferMortgageApplicationProcessorTests.cs
+++ Loan.UnitTest/FixedRateAnnuityOfferMortgageApplicationProcessorTests.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Ploeh.Samples.Loan;
+using Moq;
+using Ploeh.Samples.Loan.DataCollection;
+using Ploeh.Samples.Loan.Render;
 
 namespace Ploeh.Samples.Loan.UnitTest
 {
@@ -15,6 +18,42 @@ namespace Ploeh.Samples.Loan.UnitTest
         {
             var sut = new FixedRateAnnuityOfferMortgageApplicationProcessor();
             Assert.IsAssignableFrom<IMortgageApplicationProcessor>(sut);
+        }
+
+        [Theory]
+        [InlineData(34, "2043-06-06")]
+        [InlineData(45, "2033-01-01")]
+        public void ProduceOfferReturnsCorrectResult(
+            int rate,
+            string term)
+        {
+            var application = new MortgageApplication();
+            var offer = new FixedRateAnnuityOffer
+            {
+                Rate = rate,
+                Term = DateTimeOffset.Parse(term)
+            };
+            var sut = new FixedRateAnnuityOfferMortgageApplicationProcessor
+            {
+                OfferService = new Mock<IOfferService>().Object
+            };
+            Mock.Get(sut.OfferService)
+                .Setup(o => o.GetFixedRateAnnuityOffer(application))
+                .Returns(offer);
+
+            var actual = sut.ProduceOffer(application);
+
+            var expected = new IRendering[]
+            {
+                new Heading2Rendering("Fixed rate offer"),
+                new BoldRendering("Interest rate:"),
+                new TextRendering(" " + offer.Rate / 10m + " %"),
+                new LineBreakRendering(),
+                new BoldRendering("Term:"),
+                new TextRendering(" " + offer.Term.ToString("D")),
+                new LineBreakRendering()
+            };
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Loan/FixedRateAnnuityOfferMortgageApplicationProcessor.cs
+++ Loan/FixedRateAnnuityOfferMortgageApplicationProcessor.cs
@@ -10,9 +10,21 @@ namespace Ploeh.Samples.Loan
     public class FixedRateAnnuityOfferMortgageApplicationProcessor :
         IMortgageApplicationProcessor
     {
+        public IOfferService OfferService;
+
         public IEnumerable<IRendering> ProduceOffer(MortgageApplication application)
         {
-            yield break;
+            var offer = this.OfferService.GetFixedRateAnnuityOffer(application);
+
+            yield return new Heading2Rendering("Fixed rate offer");
+
+            yield return new BoldRendering("Interest rate:");
+            yield return new TextRendering(" " + offer.Rate / 10m + " %");
+            yield return new LineBreakRendering();
+
+            yield return new BoldRendering("Term:");
+            yield return new TextRendering(" " + offer.Term.ToString("D"));
+            yield return new LineBreakRendering();
         }
     }
 }
