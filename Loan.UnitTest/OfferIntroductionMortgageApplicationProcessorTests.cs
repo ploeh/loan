3460537446b2aaf8f772dcfd45a26423134474bb
Mark Seemann <mark@ploeh.dk>
2013-06-06 11:07:13 +0200

    Implemented OfferIntroductionMortgage[etc]

--- Loan.UnitTest/OfferIntroductionMortgageApplicationProcessorTests.cs
+++ Loan.UnitTest/OfferIntroductionMortgageApplicationProcessorTests.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Ploeh.Samples.Loan;
+using Ploeh.Samples.Loan.DataCollection;
+using Ploeh.Samples.Loan.Render;
 
 namespace Ploeh.Samples.Loan.UnitTest
 {
@@ -15,6 +17,23 @@ namespace Ploeh.Samples.Loan.UnitTest
         {
             var sut = new OfferIntroductionMortgageApplicationProcessor();
             Assert.IsAssignableFrom<IMortgageApplicationProcessor>(sut);
+        }
+
+        [Fact]
+        public void ProduceOfferReturnsCorrectResult()
+        {
+            var sut = new OfferIntroductionMortgageApplicationProcessor();
+
+            var dummyApplication = new MortgageApplication();
+            var actual = sut.ProduceOffer(dummyApplication);
+
+            var expected = new IRendering[]
+            {
+                new Heading1Rendering("Loan offer"),
+                new TextRendering("It gives us great pleasure to extend to you the following loan offer, lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                new LineBreakRendering()
+            };
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Loan/OfferIntroductionMortgageApplicationProcessor.cs
+++ Loan/OfferIntroductionMortgageApplicationProcessor.cs
@@ -11,7 +11,9 @@ namespace Ploeh.Samples.Loan
     {
         public IEnumerable<IRendering> ProduceOffer(MortgageApplication application)
         {
-            yield break;
+            yield return new Heading1Rendering("Loan offer");
+            yield return new TextRendering("It gives us great pleasure to extend to you the following loan offer, lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+            yield return new LineBreakRendering();
         }
     }
 }
