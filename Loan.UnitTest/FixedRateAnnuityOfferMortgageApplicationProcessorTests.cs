98a74d2c374faf432c8e4d554168a824145ea406
Mark Seemann <mark@ploeh.dk>
2013-06-06 11:35:09 +0200

    Introduced FixedRateAnnuityOfferMortgage[etc]
    which will produce renderings for a fixed rate annuity loan offer.

---
+++ Loan.UnitTest/FixedRateAnnuityOfferMortgageApplicationProcessorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Ploeh.Samples.Loan;
+
+namespace Ploeh.Samples.Loan.UnitTest
+{
+    public class FixedRateAnnuityOfferMortgageApplicationProcessorTests
+    {
+        [Fact]
+        public void SutIsMortgageApplicationProcessor()
+        {
+            var sut = new FixedRateAnnuityOfferMortgageApplicationProcessor();
+            Assert.IsAssignableFrom<IMortgageApplicationProcessor>(sut);
+        }
+    }
+}
