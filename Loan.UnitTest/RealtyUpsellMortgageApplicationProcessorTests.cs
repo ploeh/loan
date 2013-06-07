6564f282276cf95bd779b7b38094944f6a68596d
Mark Seemann <mark@ploeh.dk>
2013-06-07 12:35:09 +0200

    Introduced RealtyUpsellMAP,
    which can be used to print an offer for realty services, as an upsell
    effort.

---
+++ Loan.UnitTest/RealtyUpsellMortgageApplicationProcessorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Ploeh.Samples.Loan;
+
+namespace Ploeh.Samples.Loan.UnitTest
+{
+    public class RealtyUpsellMortgageApplicationProcessorTests
+    {
+        [Fact]
+        public void SutIsMortgageApplicationProcessor()
+        {
+            var sut = new RealtyUpsellMortgageApplicationProcessor();
+            Assert.IsAssignableFrom<IMortgageApplicationProcessor>(sut);
+        }
+    }
+}
