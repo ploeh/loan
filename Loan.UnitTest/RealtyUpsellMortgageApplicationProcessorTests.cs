95790506baaad293ce951cc2a23e3a01e7c39142
Mark Seemann <mark@ploeh.dk>
2013-06-07 12:43:13 +0200

    Implemented RealtyUpsellMAP.

--- Loan.UnitTest/RealtyUpsellMortgageApplicationProcessorTests.cs
+++ Loan.UnitTest/RealtyUpsellMortgageApplicationProcessorTests.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Ploeh.Samples.Loan;
+using Ploeh.Samples.Loan.DataCollection;
+using Ploeh.Samples.Loan.Render;
 
 namespace Ploeh.Samples.Loan.UnitTest
 {
@@ -15,6 +17,23 @@ namespace Ploeh.Samples.Loan.UnitTest
         {
             var sut = new RealtyUpsellMortgageApplicationProcessor();
             Assert.IsAssignableFrom<IMortgageApplicationProcessor>(sut);
+        }
+
+        [Fact]
+        public void ProduceOfferReturnsCorrectResult()
+        {
+            var sut = new RealtyUpsellMortgageApplicationProcessor();
+
+            var dummyApplication = new MortgageApplication();
+            var actual = sut.ProduceOffer(dummyApplication);
+
+            var expected = new IRendering[]
+            {
+                new Heading1Rendering("Real estate services"),
+                new TextRendering("Do you need help selling your current property? Then  lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                new LineBreakRendering()
+            };
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Loan/RealtyUpsellMortgageApplicationProcessor.cs
+++ Loan/RealtyUpsellMortgageApplicationProcessor.cs
@@ -13,7 +13,9 @@ namespace Ploeh.Samples.Loan
     {
         public IEnumerable<IRendering> ProduceOffer(MortgageApplication application)
         {
-            yield break;
+            yield return new Heading1Rendering("Real estate services");
+            yield return new TextRendering("Do you need help selling your current property? Then  lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+            yield return new LineBreakRendering();
         }
     }
 }
